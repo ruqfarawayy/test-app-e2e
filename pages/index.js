0579553fc4b9a6e7c9804f1e9847df1ae7631560
adhemukhlis <adhemukhlis@gmail.com>
2023-07-20 21:32:25 +0700

    init

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+"use client";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Button, Form, Input } from "antd";
+import { useState } from "react";
 
 export default function Home() {
+  const [targetUrl, setTargetUrl] = useState(
+    "http://104.248.154.192:4000/katalog/"
+  );
+  const handleFinish = (values) => {
+    const { targetUrl: formTargetUrl } = values;
+    setTargetUrl(formTargetUrl);
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -11,61 +21,37 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <main
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+          padding: "4rem",
+          minHeight: "100vh",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+          }}
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+          <Form
+            layout="inline"
+            initialValues={{ targetUrl }}
+            onFinish={handleFinish}
+          >
+            <Form.Item name="targetUrl">
+              <Input style={{ width: "40vw" }} />
+            </Form.Item>
+            <Form.Item>
+              <Button htmlType="submit">Go</Button>
+            </Form.Item>
+          </Form>
+        </div>
+        <iframe src={targetUrl} style={{ width: "100%", flex: 1 }}></iframe>
+      </main>
     </div>
-  )
+  );
 }
